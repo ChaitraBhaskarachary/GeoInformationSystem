0d560be9e26f59285b1092b66f0f3ca5c4bc2d93
Chaitra Bhaskarachary <38094622+chaitra2232@users.noreply.github.com>
2020-06-20 21:51:39 +0200

    Update slowtimelapse.py

--- slowtimelapse.py
+++ slowtimelapse.py
@@ -40,57 +40,57 @@ for fname in glob.glob(path):
   t4=datetime.datetime.fromtimestamp(t3) 
   s1=fname
  #stamp on gps coordinates
- if lat==’nan’:
-   latstring=’Lat: nan’
-   lonstring=’Lon: nan’
+ if lat=='nan':
+   latstring='Lat: nan'
+   lonstring='Lon: nan'
  else:
-   latstring=’Lat: ‘+str(lat)
-   lonstring=’Lon: ‘+str(lon)
+   latstring='Lat: '+str(lat)
+   lonstring='Lon: '+str(lon)
  
  #make big string at bottom
  t4s=str(t4)
- tz=ti.strftime(‘%Z’,ti.gmtime())
- bigstring=t4s+’ ‘+tz+’ | ‘+s+’ | ‘+gpsfix+’ | ‘+latstring+’ | ‘+lonstring
+ tz=ti.strftime('%Z',ti.gmtime())
+ bigstring=t4s+' '+tz+' | '+s+' | '+gpsfix+' | '+latstring+' | '+lonstring
  
  #save image with date filename
- t5=ti.strftime(‘%Y%m%d%H%M%S’,tt)
- imagename=t5+’.jpg’
- d0=’/media/usb/webcam/’+imagename
- cmdstring=”/usr/bin/convert “+s1+” -pointsize 17 -fill white -annotate +20+760 ‘”+bigstring+”’ “+d0
+ t5=ti.strftime('%Y%m%d%H%M%S',tt)
+ imagename=t5+'.jpg'
+ d0='/media/usb/webcam/'+imagename
+ cmdstring="/usr/bin/convert "+s1+" -pointsize 17 -fill white -annotate +20+760 ‘"+bigstring+"’ "+d0
  os.system(cmdstring)
  
  #Archive current image in date-time folder structure
- year=ti.strftime(‘%Y’,tt)
- month=ti.strftime(‘%m’,tt)
- day=ti.strftime(‘%d’,tt)
- hour=ti.strftime(‘%H’,tt)
+ year=ti.strftime('%Y',tt)
+ month=ti.strftime('%m',tt)
+ day=ti.strftime('%d',tt)
+ hour=ti.strftime('%H',tt)
 
  t=pytz.timezone(ti.tzname[1])
  t4aware=t.localize(t4)
- if not gpstime==’nan’:
+ if not gpstime=='nan':
   difft=gpstime-t4aware
   ss=difft.total_seconds()
  
- if ss<120 and gpsfix==’GPS time-sync: yes’ and not gpstime==’nan’:
-  d1=’/media/usb/webcam/’+year+’/’+month+’/’+day+’/’+hour+’/’+imagename
+ if ss<120 and gpsfix=='GPS time-sync: yes' and not gpstime=='nan':
+  d1='/media/usb/webcam/'+year+'/'+month+'/'+day+'/'+hour+'/'+imagename
  else:
-  d1=’/media/usb/webcam/unsuretimestamp/’+year+’/’+month+’/’+day+’/’+hour+’/’+imagename
+  d1='/media/usb/webcam/unsuretimestamp/'+year+'/'+month+'/'+day+'/'+hour+'/'+imagename
  
  d2=os.path.dirname(d1)
  #if file path does not exist, make it
  if not os.path.exists(d2):
   os.makedirs(d2)
  
- shutil.copyfile(d0,’/var/www/image.jpg’) #copy image to webserver directory
+ shutil.copyfile(d0,'/var/www/image.jpg') #copy image to webserver directory
  #print d0
  #print d1
  shutil.move(d0,d1) #move file to new date folder
 
 #write metadata text file for webserver
-f=open(‘metadata.txt’,’w’)
-f.write(t5+’\n’)
+f=open('metadata.txt','w')
+f.write(t5+'\n')
 f.close()
-shutil.move(‘metadata.txt’,’/var/www/metadata.txt’)
+shutil.move('metadata.txt','/var/www/metadata.txt')
 
 toc=ti.time()
 print(toc-tic)
